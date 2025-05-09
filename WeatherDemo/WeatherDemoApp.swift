1727d6efac917a376ed045ce9117d35efa504d44
Jed <hatdoc@naver.com>
2025-05-09 19:21:57 +0800

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Jed Park on 9/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherDemoTests/WeatherDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoTests.swift
+//  WeatherDemoTests
+//
+//  Created by Jed Park on 9/5/2025.
+//
+
+import Testing
+@testable import WeatherDemo
+
+struct WeatherDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
